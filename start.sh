7974b1190e8e68d0b974b07405db991cf5fa3b08
Philip Vorobyov <Pilip.Varabyou@aerlingus.com>
2019-05-03 13:02:59 +0200

    optimize bash script

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if ssh $HOST -p 2222 "test -e $FILE"; then
     echo "file is already uploaded to virtual box"
 else
     echo "uploading csv file virtual box"
-    scp -P 2222 /Users/philipvorobyov/devei/workspace/hdfs/src/hdfs/input_data/destinations.csv  $HOST:/srv
+    scp -P 2222 ./src/hdfs/output_data/destinations.csv  $HOST:/srv
     echo "uploading csv to hdfs"
     ssh -t $HOST -p 2222 'sudo hdfs dfs -put /srv/destinations.csv  /user/maria_dev/'
 fi
